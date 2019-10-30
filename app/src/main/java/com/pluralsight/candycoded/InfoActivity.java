7e6fe64ac38dbf0cfd3f77dbe7379b935d49d171
Gemma Usiku <afrogem3@gmail.com>
2019-10-31 03:35:13 +0800

    Task 2 project phase

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -30,12 +30,11 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     public void createMapIntent(View view) {
         Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent= new Intent(Intent.ACTION_VIEW, uriAddress);
-        mapIntent.setPackage("com.google.apps.maps");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
+        mapIntent.setPackage("com.google.android.apps.maps");
 
         if (mapIntent.resolveActivity(getPackageManager()) !=null) {
             startActivity(mapIntent);
-
         }
 
     }
